b2ea616c0941657ebf8ea074a834da91934eb76a
rkrobby19 <rizkyrobby19@gmail.com>
2023-03-14 09:04:58 +0700

    Add categories query and change component of categories and header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-
-const categories = [
-  { name: "Web Development", slug: "web-dev" },
-  { name: "React", slug: "react" },
-];
+import { getCategories } from "@/services";
 
 function Header() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
